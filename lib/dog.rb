a8e3ac700f9399ac9c6b3132fcbe96a8e64d18e4
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-17 01:43:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog
   
   def initialize(dog_name, dog_breed = "Mutt")
-    @name = name
-    @breed = breed
+    @name = dog_name
+    @breed = dog_breed
   end
   
   def name=(dog_name, dog_breed = "Mutt")
